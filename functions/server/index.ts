ad035a3cc0e3e37d6ff89e360c3362161d84d359
Fabrizzio Aron Aranda La Rosa <faranda@belatrix.com>
2020-01-10 15:45:35 -0500

    TODO: configure swagger

--- functions/server/index.ts
+++ functions/server/index.ts
@@ -3,6 +3,8 @@ import cors from 'cors';
 import morgan from 'morgan';
 import fileSystem from 'fs';
 import path from 'path';
+import swaggerUI from 'swagger-ui-express';
+import * as swaggerDocument from './swagger.json';
 // importing routes
 import mainController from './controllers';
 const app = express();
@@ -21,6 +23,7 @@ app.use(morgan('combined', {
 }));
 // Routes
 app.use('/api', mainController);
+app.use('/api-docs',swaggerUI.serve, swaggerUI.setup(swaggerDocument, {explorer: true}));
 
 // Starting the server
 
